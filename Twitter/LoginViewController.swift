b60fb9d7a8cb34ba46b08786b4b13508984b0877
roneil <roneilsembrano@gmail.com>
2020-03-05 13:35:33 -0800

    added assets and did some stuff for login

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Roneil Sembrano on 3/5/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func OnLoginButton(_ sender: Any) {
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "LoginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not login")
+        })
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()//what url do u want to call, what happens when it works, when it doesn't?
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
