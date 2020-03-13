79eca57760c3760b31e4ed648c772bc53bbaaed5
roneil <roneilsembrano@gmail.com>
2020-03-12 23:44:03 -0700

    able to retweet now completed all required user stories

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -75,6 +75,8 @@ class HomeTableViewController: UITableViewController {
         cell.setFavorite(isFavorited: tweetArray[indexPath.row]["favorited"] as! Bool)
 //        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(isRetweeted: tweetArray[indexPath.row]["retweeted"] as! Bool)
+  
         return cell
     }
 
